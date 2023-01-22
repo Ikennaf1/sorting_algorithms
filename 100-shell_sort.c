ed1c3d62d40a002f4474f316ea4c84117c5d82dc
Ikennaf1 <iiikbros202@gmail.com>
2023-01-22 13:44:36 +0000

    Shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,5 +7,33 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	//
+	int /*lb, ub,*/ temp, i, j, gap = 1, a_size = size;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (gap < (a_size / 3))
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap = (gap - 1) / 3)
+	{
+		for (j = gap; j < a_size; j++)
+		{
+			for (i = j - gap; i >= 0; i -= gap)
+			{
+				if (array[i + gap] > array[i])
+				{
+					break;
+				}
+				else
+				{
+					temp = array[i + gap];
+					array[i + gap] = array[i];
+					array[i] = temp;
+					/*print_array(array, size);*/
+				}
+			}
+		}
+		print_array(array, size);
+	}
 }
